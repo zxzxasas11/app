94f8c5c8a29b4d3e243ffd5980994f22c672d666
Administrator <zxzx>
2019-06-11 18:05:49 +0800

    文件上传

--- app/controllers/uploadFile.js
+++ app/controllers/uploadFile.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 class UploadFileController {
     /**
      * 上传文件
@@ -13,6 +14,20 @@ class UploadFileController {
         const upStream = fs.createWriteStream(`upload/${Math.random().toString()}.${ext}`); // 创建可写流
         reader.pipe(upStream); // 可读流通过管道写入可写流
         return ctx.body = '上传成功';
+        // 上传单个文件
+        /*const file = ctx.request.files.file; // 获取上传文件
+        console.log(file.path);
+        // 创建可读流
+        const reader = fs.createReadStream(file.path);
+        let filePath = path.join(__dirname, 'public/upload/') + `/${file.name}`;
+        if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath,'0777');
+        }
+        // 创建可写流
+        const upStream = fs.createWriteStream(filePath);
+        // 可读流通过管道写入可写流
+        reader.pipe(upStream);
+        return ctx.body = "上传成功！";*/
     }
 
 
